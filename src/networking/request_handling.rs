dff5f66795cede86f6da322b74394be0c10560fc
EineNachtImMai <titouan.cazaubieilh@proton.me>
2025-11-09 22:03:21 +0100

    chore: in the process of migrating the request handling to hyper.rs

--- src/networking/request_handling.rs
+++ src/networking/request_handling.rs
@@ -8,6 +8,23 @@ use clap::Parser;
 
 use crate::calendar_parsing::parsing::get_calendar;
 
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+
+async fn handle_conn() {
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle_connection(stream, calendar_list, req)))
+    });
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
 pub fn serve(calendar_list: Vec<String>) {
     let args = Args::parse();
     let port = args.port;
@@ -23,15 +40,11 @@ pub fn serve(calendar_list: Vec<String>) {
     }
 }
 
-fn handle_connection(mut stream: TcpStream, calendar_list: Vec<String>) {
-    let buf_reader = BufReader::new(&stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!["{:?}", _http_request];
+async fn handle_connection(
+    calendar_list: Vec<String>,
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
+    println!["{:?}", req];
 
     let content = format!["{}", get_calendar(calendar_list)];
     let length = content.len();
@@ -39,4 +52,10 @@ fn handle_connection(mut stream: TcpStream, calendar_list: Vec<String>) {
     let response = format!["HTTP/1.1 200 OK\r\nContent-Type: text/calendar;charset=UTF-8\r\nContent-Length: {length}\r\nContent-Disposition: inline; filename=ADECal.ics\r\n\r\n{content}"];
 
     stream.write_all(response.as_bytes()).unwrap();
+
+    Ok(Response::builder()
+        .header("Content-Type", "text/calendar;charset=UTF-8")
+        .header("Content-Disposition", "inline; filename=ADECal.ics")
+        .body(Body::from(content))
+        .unwrap())
 }
